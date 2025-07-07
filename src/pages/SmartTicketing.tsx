9a3fe961eedf199596e8907adaa93bf4458a86b0
Chibuike Maduabuchi <chibuikemaduabuchi2023@gmail.com>
2025-07-07 05:13:22 +0100

    updates

--- src/pages/SmartTicketing.tsx
+++ src/pages/SmartTicketing.tsx
@@ -1,6 +1,19 @@
 import { CancelIcon } from "../assets/icons";
 import Button from "../components/Button";
 import heroImage from "../assets/smart-ticketing/hero-image.webp";
+import firstImage from "../assets/smart-ticketing/first.webp";
+import secondImage from "../assets/smart-ticketing/second.webp";
+import thirdImage from "../assets/smart-ticketing/third.webp";
+import fourthImage from "../assets/smart-ticketing/fourth.webp";
+import fifthImage from "../assets/smart-ticketing/fifth.webp";
+import sixthImage from "../assets/smart-ticketing/sixth.webp";
+import seventhImage from "../assets/smart-ticketing/seventh.webp";
+import eighthImage from "../assets/smart-ticketing/eighth.webp";
+import ninthImage from "../assets/smart-ticketing/ninth.webp";
+import tenthImage from "../assets/smart-ticketing/tenth.webp";
+import eleventhImage from "../assets/smart-ticketing/eleventh.webp";
+import twelfthImage from "../assets/smart-ticketing/twelfth.webp";
+import thirteenthImage from "../assets/smart-ticketing/thirteenth.webp";
 
 export default function SmartTicketing() {
 	return (
@@ -73,6 +86,78 @@ export default function SmartTicketing() {
 					</div>
 				</div>
 			</section>
+			<section className="mt-[52px] md:mt-[104px]">
+				<img
+					src={firstImage}
+					alt="Image of three screens of the app design"
+					width={1296}
+					height={750}
+				/>
+				<p className="w-full max-w-[855px] font-medium text-[14px] md:text-[16px] text-gray-500 leading-[1.6] mt-6 md:mt-12">
+					For the entry screen, I drew inspiration from Airbnb’s card layout and applied a
+					content-first approach that prioritizes scannability. Each card highlights key details
+					like date, location, and availability at a glance. The design uses clear visual hierarchy
+					and spacing to guide users towards their desired goal with less friction.
+				</p>
+			</section>
+			<section className="mt-[52px] md:mt-[104px]">
+				<img
+					src={secondImage}
+					alt="Image of three screens of the app design"
+					width={1296}
+					height={750}
+				/>
+				<p className="w-full max-w-[855px] font-medium text-[14px] md:text-[16px] text-gray-500 leading-[1.6] mt-6 md:mt-12">
+					Key event details are shown clearly, whether it's a one-day show or a multi-day
+					experience. The focus was on helping users quickly understand what’s happening and when,
+					so they can make informed decisions easily.
+				</p>
+			</section>
+			<section className="mt-[52px] md:mt-[104px]">
+				<img
+					src={thirdImage}
+					alt="Image of three screens of the app design"
+					width={1296}
+					height={750}
+				/>
+				<p className="w-full max-w-[855px] font-medium text-[14px] md:text-[16px] text-gray-500 leading-[1.6] mt-6 md:mt-12">
+					I designed a low-friction selection flow that encourages quick decisions through clear
+					options and immediate visual feedback.
+				</p>
+			</section>
+			{[fourthImage, fifthImage, sixthImage, seventhImage, eighthImage].map((image, index) => (
+				<img
+					key={index}
+					src={image}
+					alt="Image of two screens of the app design"
+					width={1296}
+					height={750}
+					className="mt-[52px] md:mt-[104px]"
+				/>
+			))}
+			<section className="mt-[52px] md:mt-[104px]">
+				<p className="w-full max-w-[855px] font-medium text-[14px] md:text-[16px] text-gray-500 leading-[1.6] mb-6 md:mb-12">
+					Unfortunately, the entire team was let go, and as a result, much of my work was ultimately
+					shelved. Management decided to bring in external contractors to redesign and rebuild the
+					app from scratch. Below are some of my contributions to the entire mobile app redesign.
+				</p>
+				<img
+					src={ninthImage}
+					alt="Image of three screens of the app design"
+					width={1296}
+					height={750}
+				/>
+			</section>
+			{[tenthImage, eleventhImage, twelfthImage, thirteenthImage].map((image, index) => (
+				<img
+					key={index}
+					src={image}
+					alt="Image of two screens of the app design"
+					width={1296}
+					height={750}
+					className={`mt-[52px] md:mt-[104px] ${index === 3 && "mb-[52px] md:mb-[104px]"}`}
+				/>
+			))}
 		</main>
 	);
 }
